40af258bc76e86595ed4d42ab0b335af3307c916
saikiran2111 <saikiran211101@gmail.com>
2023-11-03 01:29:26 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,150 @@
+import {Component} from 'react'
+import './App.css'
+import UserHistory from './components/UserHistory'
+
+// These are the list used in the application. You can move them to any component needed.
+const initialHistoryList = [
+  {
+    id: 0,
+    timeAccessed: '07:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/instagram-img.png',
+    title: 'Instagram',
+    domainUrl: 'instagram.com',
+  },
+  {
+    id: 1,
+    timeAccessed: '05:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/twitter-img.png',
+    title: 'Twitter. It’s what’s happening / Twitter',
+    domainUrl: 'twitter.com',
+  },
+  {
+    id: 2,
+    timeAccessed: '04:35 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/facebook-img.png',
+    title: 'Facebook – log in or sign up',
+    domainUrl: 'facebook.com',
+  },
+  {
+    id: 3,
+    timeAccessed: '04:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/linkedin-img.png',
+    title: 'LinkedIn: Log In or Sign Up',
+    domainUrl: 'linkedin.com',
+  },
+  {
+    id: 4,
+    timeAccessed: '04:00 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/hashnode-img.png',
+    title: 'Hashnode: Everything you need to start blogging as a developer!',
+    domainUrl: 'hashnode.com',
+  },
+  {
+    id: 5,
+    timeAccessed: '03:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/github-img.png',
+    title: 'GitHub: Where the world builds software · GitHub',
+    domainUrl: 'github.com',
+  },
+
+  {
+    id: 6,
+    timeAccessed: '02:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/react-img.png',
+    title: 'React – A JavaScript library for building user interfaces',
+    domainUrl: 'reactjs.org',
+  },
+  {
+    id: 7,
+    timeAccessed: '01:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/stackoverflow-img.png',
+    title: 'Stack Overflow - Where Developers Learn, Share, & Build Careers',
+    domainUrl: 'stackoverflow.com',
+  },
+
+  {
+    id: 8,
+    timeAccessed: '09:25 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/gmail-img.png',
+    title: 'Gmail',
+    domainUrl: 'mail.google.com',
+  },
+  {
+    id: 9,
+    timeAccessed: '09:00 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/google-img.png',
+    title: 'Google',
+    domainUrl: 'google.com',
+  },
+]
+
+// Replace your code here
+class App extends Component {
+  state = {
+    searchInput: '',
+    userHistoryList: initialHistoryList,
+  }
+
+  onSearchValue = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  deleteItem = id => {
+    const {userHistoryList} = this.state
+    const filteredUserList = userHistoryList.filter(each => each.id !== id)
+
+    this.setState({userHistoryList: filteredUserList})
+  }
+
+  render() {
+    const {searchInput, userHistoryList} = this.state
+    // console.log(searchInput)
+
+    const searchResults = userHistoryList.filter(eachItem =>
+      eachItem.title.includes(searchInput),
+    )
+
+    return (
+      <div className="main-container">
+        <div className="navbar">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png"
+            alt="app logo"
+            className="app-logo"
+          />
+          <div className="search-container">
+            <div className="search-img-container">
+              <img
+                className="search-img"
+                src="https://assets.ccbp.in/frontend/react-js/search-img.png"
+                alt="search"
+              />
+            </div>
+            <input
+              type="search"
+              placeholder="Search History"
+              value={searchInput}
+              onChange={this.onSearchValue}
+              className="searchInputContainer"
+            />
+          </div>
+        </div>
+        <div className="content-container">
+          <div className="inner-container">
+            <ul className="list-container">
+              {searchResults.map(eachResult => (
+                <UserHistory
+                  key={eachResult.id}
+                  historyDetails={eachResult}
+                  deleteItem={this.deleteItem}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/UserHistory/index.js
@@ -0,0 +1,33 @@
+import './index.css'
+
+const UserHistory = props => {
+  const {historyDetails, deleteItem} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = historyDetails
+
+  const onDelete = () => {
+    deleteItem(id)
+  }
+
+  return (
+    <li className="list-item">
+      <div className="written-content">
+        <p className="time">{timeAccessed}</p>
+        <img src={logoUrl} alt="domain logo" className="list-logo" />
+        <p className="list-title">{title}</p>
+        <p className="list-url">{domainUrl}</p>
+      </div>
+      <div className="delete-container">
+        <button data-testid="delete" className="delete-button" type="button">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png "
+            alt="delete"
+            className="delete-icon"
+            onClick={onDelete}
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default UserHistory
